d710c4fc9461850d6f5095711b196770797bf2cb
prmstrgt <pramestiregitaa@gmail.com>
2020-09-09 12:51:27 +0700

    revisi path /data with POST method on index.js

--- index.js
+++ index.js
@@ -2,21 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/home', (req, res)=>{
     res.send('Welcome to my first backend!')
 })
 
-app.get('/main', (req, res)=>{
-    res.sendFile('/media/user/data1/b172/minggu4/backend/html/form.html')
-})
-
 app.post('/data', (req, res)=>{
     console.log(req.body)
-    res.sendStatus(200)
 })
 
 app.listen(8080, ()=>{
