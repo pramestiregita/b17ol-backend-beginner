6dc789b52a1661f9cb4d8de58b4ad3ddfd8673d3
prmstrgt <pramestiregitaa@gmail.com>
2020-09-09 08:56:03 +0700

    create path /data with POST method on index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/home', (req, res)=>{
     res.send('Welcome to my first backend!')
+})
+
+app.get('/main', (req, res)=>{
+    res.sendFile('/media/user/data1/b172/minggu4/backend/html/form.html')
+})
+
+app.post('/data', (req, res)=>{
+    console.log(req.body)
+    res.sendStatus(200)
 })
 
 app.listen(8080, ()=>{
